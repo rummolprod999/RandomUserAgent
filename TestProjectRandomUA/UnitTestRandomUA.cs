9d32f476b6cb0ecc278cca752d321827d220d853
rummolprod999 <rummolprod999@gmail.com>
2020-01-28 13:29:19 +0300

    update

---
+++ TestProjectRandomUA/UnitTestRandomUA.cs
@@ -0,0 +1,28 @@
+using System;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace TestProjectRandomUA
+{
+    public class UnitTest1
+    {
+        private readonly ITestOutputHelper _testOutputHelper;
+
+        public UnitTest1(ITestOutputHelper testOutputHelper)
+        {
+            _testOutputHelper = testOutputHelper;
+        }
+
+        [Fact]
+        public void Test1()
+        {
+            for (var i = 0; i <= 500; i++)
+            {
+                var ua = RandomUserAgent.RandomUA.randomUserAgent;
+                _testOutputHelper.WriteLine(ua);
+                Assert.True(ua != "");
+            }
+            
+        }
+    }
+}
